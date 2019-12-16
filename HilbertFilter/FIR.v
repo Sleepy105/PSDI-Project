5619c5e26249a6c1d11734c37a4b534406adaf89
Sleepy105 <lm.sousa@fe.up.pt>
2019-12-16 12:16:15 +0000

    idk cenas

--- HilbertFilter/FIR.v
+++ HilbertFilter/FIR.v
@@ -1,7 +1,7 @@
 `timescale 10ns / 10ns
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: IEEE University of Porto Student Branch
+// Engineer: Luís Sousa & Sílvia Faria
 // 
 // Create Date:    15:52:28 11/26/2019 
 // Design Name: 
@@ -18,53 +18,75 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module FIR
-	#(parameter total_bits=12) (
+module FIR (
     input clock,
     input reset,
-    input [total_bits-1:0] IN,
-    output [total_bits-1:0] Re,
-    output [total_bits-1:0] Im
+	 input EN,
+    input signed [11:0] IN,		// 12 integer bits
+    output signed [12:0] Re,		// 13 integer bits
+    output signed [12:0] Im		// 13 integer bits
     );
 	
-	parameter order = 8;
-	parameter [total_bits-1:0] coef1 = 12'b0001_1110_1000; // 0.23828125
-	parameter [total_bits-1:0] coef3 = 12'b0101_0000_0000; // 0.625
+	parameter order = 4; // order 8, but 4 coefs are '0'
+	parameter [25:0] coef1 = 26'b0_0000_0000_0000__0011_1101_0000_0; // 0.23828125
+	parameter [25:0] coef2 = 26'b0_0000_0000_0000__1010_0000_0000_0; // 0.62500000
+	parameter cadence = 20-1; // Take an input every 20 clock cycles
 	
-	wire [total_bits-1:0] coef1pos = IN*coef1;
-	wire [total_bits-1:0] coef3pos = IN*coef3;
-	
-	reg [3:0] i;
-	reg [total_bits-1:0] FF_re [order:0];
-	reg [total_bits-1:0] FF_conv [order-1:0];
 
-	assign Re = FF_re[order];
-	assign Im = FF_conv[order-1];
+	reg [4:0] cnt;							// Counter from 19 to 0
+	reg [2:0] i;							// 0 to 3 (order = 4)
+	reg [11:0] IN_reg;					// 12 int
+	reg [11:0] FF_re [order-1:0];		// 12 int
+	reg [25:0] FF_conv [order-1:0];	// 13 int + 13 frac
+	
+	wire [51:0] multBuffer1 = {IN_reg[11], IN_reg, 13'd0}*coef1;	// 26 int + 26 frac
+	wire [51:0] multBuffer2 = {IN_reg[11], IN_reg, 13'd0}*coef2;	// 26 int + 26 frac
+	wire [25:0] coef1pos = multBuffer1[38:13];	// 13 int + 13 frac
+	wire [25:0] coef2pos = multBuffer2[38:13];	// 13 int + 13 frac
+
+	assign Re = {FF_re[order-1][11], FF_re[order-1]};
+	assign Im = FF_conv[order-1][25:13];
+	
+	initial begin
+		i = 0;
+		IN_reg = 0;
+		cnt = cadence;
+		for (i=0; i < order; i = i+1) begin
+			FF_re[i] = 0;
+			FF_conv[i] = 0;
+		end
+	end
 	
 	always @(posedge clock) begin
+		IN_reg <= IN;
+			
 		if (reset) begin
+			// Reset all registers
+			IN_reg <= 0;
+			cnt <= cadence;
 			for (i=0; i < order; i = i+1) begin
+				FF_re[i] <= 0;
 				FF_conv[i] <= 0;
 			end
-			for (i=0; i < order+1; i = i+1) begin
-				FF_re[i] <= 0;
-			end
 		end
-		else begin
-			FF_re[0] <= IN;
-		
-			for (i=1; i < order+1; i = i+1) begin
-				FF_re[i] <= FF_re[i-1];
+		else if (EN) begin
+			if (cnt) begin
+				cnt <= cnt - 5'd1;
 			end
+			else begin
+				cnt <= cadence;
+				
+				FF_re[0] <= IN_reg;
 			
-			FF_conv[0] <= -coef1pos;
-			FF_conv[1] <= FF_conv[0];
-			FF_conv[2] <= FF_conv[1] - coef3pos;
-			FF_conv[3] <= FF_conv[2];
-			FF_conv[4] <= FF_conv[3] + coef3pos;
-			FF_conv[5] <= FF_conv[4];
-			FF_conv[6] <= FF_conv[5] + coef1pos;
-			FF_conv[7] <= FF_conv[6];
+				for (i=1; i < order; i = i+1) begin
+					FF_re[i] <= FF_re[i-1];
+				end
+				
+				FF_conv[0] <= -coef1pos;
+				FF_conv[1] <= FF_conv[0] - coef2pos;
+				FF_conv[2] <= FF_conv[1] + coef2pos;
+				FF_conv[3] <= FF_conv[2] + coef1pos;
+			end
 		end
 	end
 
--- HilbertFilter/FIR_tb.v
+++ HilbertFilter/FIR_tb.v
@@ -24,29 +24,46 @@
 
 module FIR_tb;
 
-	parameter CLOCK_PERIOD = 500; // ns
+	parameter CLOCK_PERIOD = 10; // ns
 
 	// Inputs
 	reg clock;
 	reg [11:0] IN;
 	reg reset;
+	reg EN;
 
 	// Outputs
-	wire [11:0] Re;
-	wire [11:0] Im;
+	wire [12:0] Re;
+	wire [12:0] Im;
 
 	// Instantiate the Unit Under Test (UUT)
-	FIR #(12) uut (
+	FIR uut (
 		.clock(clock), 
-		.IN(IN), 
+		.IN(IN),
+		.EN(EN),
 		.reset(reset), 
 		.Re(Re), 
 		.Im(Im)
 	);
 	
+	integer               data_file1    ; // file handler
+	integer               scan_file1    ; // file handler
+	integer               data_file2    ; // file handler
+	integer               scan_file2    ; // file handler
+	integer               data_file3    ; // file handler
+	integer               scan_file3    ; // file handler
+	real error;
+	reg   [11:0] rxuw;
+	reg   [11:0] realuw;
+	reg   [11:0] imaguw;
+	reg	[4:0]	 cnt;
+	`define NULL 0
+	
+	parameter cadence = 20-1;
+	
 	initial begin
 		// Generate the clock signal:
-		forever #(CLOCK_PERIOD/2) clock = ~clock;
+		forever #(CLOCK_PERIOD) clock = ~clock;
 	end
 
 	initial begin
@@ -54,9 +71,20 @@ module FIR_tb;
 		clock = 0;
 		IN = 0;
 		reset = 0;
+		EN = 0;
+		cnt = cadence;
 
 		// Wait 100 ns for global reset to finish
-		#100;
+		#50;
+		$display("Starting\n");
+		
+		data_file1 = $fopen("text1.txt", "r");
+		data_file2 = $fopen("text2.txt", "r");
+		data_file3 = $fopen("text3.txt", "r");
+		if (data_file1 == `NULL) begin
+			$display("data_file handle was NULL");
+			$finish;
+		end
         
 		// Add stimulus here
 		@(posedge clock);
@@ -65,9 +93,41 @@ module FIR_tb;
 		@(posedge clock);
 		#10
 		reset = 0;
+		EN = 1;
+		scan_file1 = $fscanf(data_file1, "%b\n", rxuw);
+		scan_file2 = $fscanf(data_file2, "%b\n", realuw);
+		scan_file3 = $fscanf(data_file3, "%b\n", imaguw);
 	end
+
+	always @(posedge clock) begin
+		if (EN) begin
+			if (cnt) begin
+				cnt <= cnt - 5'd1;
+			end
+			else begin
+				cnt <= cadence;
+			
+				scan_file1 = $fscanf(data_file1, "%b\n", rxuw);
+				scan_file2 = $fscanf(data_file2, "%b\n", realuw);
+				scan_file3 = $fscanf(data_file3, "%b\n", imaguw);
+				IN = rxuw;
+
+				if (!$feof(data_file1)) begin
+					$display("[file]->%x,%x,%x", rxuw, {realuw[11], realuw}, {imaguw[11], imaguw});
+					#1
+					error = 100 * ( {imaguw[11], imaguw} - Im ) / Im;
+					$display("[firm]->%x,%x,%x --> delta: %f%%\n", IN, Re, Im, error);
+				end
+				else begin
+					$finish;
+				end
+			end
+		end
+	end
+
 	
-	initial begin
+	
+	/*initial begin
 		#10
 		@(negedge reset);
 		repeat(10)
@@ -85,7 +145,7 @@ module FIR_tb;
 		end
 		
 		$finish;
-	end
+	end*/
       
 endmodule
 
